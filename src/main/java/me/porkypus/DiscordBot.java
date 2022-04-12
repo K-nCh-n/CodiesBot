174520655d49e76c3ee1ef6470351c704bd44c83
Ronan Ragavoodoo <rdr32@cam.ac.uk>
2022-04-12 20:38:25 +0100

    Made it a bit better

--- src/main/java/me/porkypus/DiscordBot.java
+++ src/main/java/me/porkypus/DiscordBot.java
@@ -21,6 +21,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 
 public class DiscordBot extends ListenerAdapter {
@@ -33,6 +34,7 @@ public class DiscordBot extends ListenerAdapter {
     static List<Button> spymasterButtonList = new ArrayList<>();
     static List<Button> playerButtonList = new ArrayList<>();
 
+    static List<User> pool = new ArrayList<>();
     static List<User> spymaster = new ArrayList<>();
     static List<User> red = new ArrayList<>();
     static List<User> blue = new ArrayList<>();
@@ -48,6 +50,7 @@ public class DiscordBot extends ListenerAdapter {
                 .build();
 
         jda.upsertCommand("codies", "Type /codies").queue();
+        jda.upsertCommand("start", "Type /start").queue();
 
         try {
             BufferedReader br = new BufferedReader(new FileReader("words.txt"));
@@ -67,137 +70,130 @@ public class DiscordBot extends ListenerAdapter {
         }
         Message msg = event.getMessage();
 
-        if (msg.getContentRaw().equals("!spymaster")) {
+        if (msg.getContentRaw().equals("!join")) {
             msg.delete().queue();
-            if (!running) {
-                event.getChannel().sendMessage("Please start a game to join a team").queue();
+            if (pool.contains(event.getAuthor())) {
+                event.getChannel().sendMessage("Join only once idiot").queue();
             } else {
-                if (spymaster.size() < 2) {
-                    if (spymaster.contains(event.getAuthor())) {
-                        event.getChannel().sendMessage(event.getAuthor().getName() + " is already a spymaster").queue();
-                    } else {
-                        spymaster.add(event.getAuthor());
-                        event.getChannel().sendMessage( "```" + event.getAuthor().getName() + " has been added to the spymasters```").queue();
-                    }
-                } else {
-                    event.getChannel().sendMessage("There can only be two spymasters").queue();
-                }
-            }
-        }
-
-        if (msg.getContentRaw().equals("!red")) {
-            msg.delete().queue();
-            if (!running) {
-                event.getChannel().sendMessage("Please start a game to join a team").queue();
-            } else {
-                if (red.contains(event.getAuthor())) {
-                    event.getChannel().sendMessage(event.getAuthor().getName() + " is already on the red team").queue();
-                } else {
-                    red.add(event.getAuthor());
-                    event.getChannel().sendMessage( "```" + event.getAuthor().getName() + " has been added to the red team```").queue();
-                }
-
-            }
-        }
-
-        if (msg.getContentRaw().equals("!blue")) {
-            msg.delete().queue();
-            if (!running) {
-                event.getChannel().sendMessage("Please start a game to join a team").queue();
-            } else {
-                if (blue.contains(event.getAuthor())) {
-                    event.getChannel().sendMessage(event.getAuthor().getName() + " is already on the blue team").queue();
-                } else {
-                    blue.add(event.getAuthor());
-                    event.getChannel().sendMessage( "```" + event.getAuthor().getName() + " has been added to the blue team```").queue();
-                }
-
-            }
-        }
-
-        if (msg.getContentRaw().length() > 6) {
-            if (msg.getContentRaw().startsWith("!clue ")) {
-                guesses = Integer.parseInt(msg.getContentRaw().substring(6));
+                    pool.add(event.getAuthor());
             }
         }
     }
 
+
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
         if (event.getName().equals("codies")) {
-
+            if (running) {
+                event.reply("An instance of codies has already been started").queue();
+            }
             running = true;
-            redRemaining = 9;
-            blueRemaining = 8;
+            pool = new ArrayList<>();
+            event.reply("Spymasters: " + spymaster + "\nRed: " + red + "\nBlue: " + blue)
+                    .addActionRow(Button.primary("random", "Randomise Teams"))
+                    .queue();
+        }
 
-            spymasterButtonList = new ArrayList<>();
-            playerButtonList = new ArrayList<>();
+        if (event.getName().equals("start")) {
+            if (!running) {
+                event.reply("```Please initiate teams using /codies first").queue();
+            } else {
+                redRemaining = 9;
+                blueRemaining = 8;
 
-            Guild guild = event.getGuild();
+                spymasterButtonList = new ArrayList<>();
+                playerButtonList = new ArrayList<>();
 
-            Collections.shuffle(wordList);
+                Guild guild = event.getGuild();
+                assert guild != null;
+                TextChannel textChannel = guild.getTextChannelsByName("spymasters", true).get(0);
 
-            for (int i = 0; i < 25; i++) {
-                if (i < 8) {
-                    spymasterButtonList.add(Button.primary(wordList.get(i), wordList.get(i)).asDisabled());
-                } else if (i < 17) {
-                    spymasterButtonList.add(Button.danger(wordList.get(i), wordList.get(i)).asDisabled());
-                } else {
-                    spymasterButtonList.add(Button.secondary(wordList.get(i), wordList.get(i)).asDisabled());
+                Collections.shuffle(wordList);
+
+                for (int i = 0; i < 25; i++) {
+                    if (i < 8) {
+                        spymasterButtonList.add(Button.primary(wordList.get(i), wordList.get(i)).asDisabled());
+                    } else if (i < 17) {
+                        spymasterButtonList.add(Button.danger(wordList.get(i), wordList.get(i)).asDisabled());
+                    } else {
+                        spymasterButtonList.add(Button.secondary(wordList.get(i), wordList.get(i)).asDisabled());
+                    }
                 }
+
+                Collections.shuffle(spymasterButtonList);
+
+                for (Button button : spymasterButtonList) {
+                    playerButtonList.add(Button.success(button.getId() + "x", button.getLabel()));
+                }
+
+
+                List<ActionRow> actionRows = new ArrayList<>();
+                actionRows.add(ActionRow.of(spymasterButtonList.subList(0, 5)));
+                actionRows.add(ActionRow.of(spymasterButtonList.subList(5, 10)));
+                actionRows.add(ActionRow.of(spymasterButtonList.subList(10, 15)));
+                actionRows.add(ActionRow.of(spymasterButtonList.subList(15, 20)));
+                actionRows.add(ActionRow.of(spymasterButtonList.subList(20, 25)));
+
+                textChannel.sendMessage("True Layout")
+                        .setActionRows(actionRows)
+                        .queue();
+
+                event.reply("```Red: " + redRemaining + " Blue: " + blueRemaining + "```")
+                        .addActionRow(
+                                playerButtonList.subList(0, 5))
+                        .addActionRow(
+                                playerButtonList.subList(5, 10))
+                        .addActionRow(
+                                playerButtonList.subList(10, 15))
+                        .addActionRow(
+                                playerButtonList.subList(15, 20))
+                        .addActionRow(
+                                playerButtonList.subList(20, 25))
+                        .queue();
             }
-
-            Collections.shuffle(spymasterButtonList);
-
-            for (Button button : spymasterButtonList) {
-                playerButtonList.add(Button.success(button.getId() + "x", button.getLabel()));
-            }
-
-            assert guild != null;
-            TextChannel textChannel = guild.getTextChannelsByName("spymasters", true).get(0);
-
-            List<ActionRow> actionRows = new ArrayList<>();
-            actionRows.add(ActionRow.of(spymasterButtonList.subList(0, 5)));
-            actionRows.add(ActionRow.of(spymasterButtonList.subList(5, 10)));
-            actionRows.add(ActionRow.of(spymasterButtonList.subList(10, 15)));
-            actionRows.add(ActionRow.of(spymasterButtonList.subList(15, 20)));
-            actionRows.add(ActionRow.of(spymasterButtonList.subList(20, 25)));
-
-            textChannel.sendMessage("True Layout")
-                    .setActionRows(actionRows)
-                    .queue();
-
-            event.reply("```Red: " + redRemaining + " Blue: " + blueRemaining + "```")
-                    .addActionRow(
-                            playerButtonList.subList(0, 5))
-                    .addActionRow(
-                            playerButtonList.subList(5, 10))
-                    .addActionRow(
-                            playerButtonList.subList(10, 15))
-                    .addActionRow(
-                            playerButtonList.subList(15, 20))
-                    .addActionRow(
-                            playerButtonList.subList(20, 25))
-                    .queue();
-
         }
     }
 
     @Override
     public void onButtonInteraction(ButtonInteractionEvent event) {
 
-
-        if ((red.contains(event.getUser()) && turn != 0) || (blue.contains(event.getUser()) && turn != 0)) return;
-        if (!red.contains(event.getUser()) && !blue.contains(event.getUser())) return;
+        if (Objects.equals(event.getButton().getId(), "random")) {
+            if (pool.size() < 4) {
+                event.getChannel().sendMessage("```More than 3 players are required```").queue();
+            }
+            Collections.shuffle(pool);
+            for (int i = 0; i < pool.size();i++) {
+                if (i < 2) {
+                    spymaster.add(pool.get(i));
+                }
+                else if (i < ((pool.size() - 2) / 2) + 2) {
+                    red.add(pool.get(i));
+                }
+                else {
+                    blue.add(pool.get(i));
+                }
+            }
+            event.getMessage().editMessage("Spymasters: " + spymaster + "\nRed: " + red + "\nBlue: " + blue).queue();
+            event.editButton(event.getButton().asDisabled()).queue();
+        }
 
         for (Button button : spymasterButtonList) {
             if ((button.getId() + "x").equals(event.getButton().getId())) {
+                if (turn == 0 && !red.contains(event.getUser())) {
+                    event.deferEdit().queue();
+                    return;
+                }
+                if (turn == 1 && !blue.contains(event.getUser())) {
+                    event.deferEdit().queue();
+                    return;
+                }
+
                 if (button.getStyle().equals(ButtonStyle.DANGER)) {
                     event.editButton(event.getButton().asDisabled().withStyle(ButtonStyle.DANGER)).queue();
                     redRemaining--;
                     event.getMessage().editMessage("```Red: " + redRemaining + " Blue: " + blueRemaining + "```").queue();
                     if (redRemaining == 0) {
-                        event.getChannel().sendMessage("The Red Team has won!").queue();
+                        event.reply("The Red Team has won!").queue();
                         running = false;
                         return;
                     }
@@ -208,7 +204,7 @@ public class DiscordBot extends ListenerAdapter {
                     blueRemaining--;
                     event.getMessage().editMessage("```Red: " + redRemaining + " Blue: " + blueRemaining + "```").queue();
                     if (blueRemaining == 0) {
-                        event.getChannel().sendMessage("The Blue Team has won!").queue();
+                        event.reply("The Blue Team has won!").queue();
                         running = false;
                         return;
                     }
@@ -225,6 +221,5 @@ public class DiscordBot extends ListenerAdapter {
             else turn = 0;
             guesses = 1;
         }
-
     }
 }
