214da75017809a6821acb0188dd74a0777259e3f
Ken <echan123@uottawa.ca>
2022-04-16 15:31:25 -0400

    Fix Randomness

--- src/main/java/me/porkypus/Codenames.java
+++ src/main/java/me/porkypus/Codenames.java
@@ -2,6 +2,7 @@ package me.porkypus;
 
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.interactions.components.buttons.ButtonStyle;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -10,16 +11,16 @@ import java.util.*;
 public class Codenames {
 
     HashSet<String> wordList, customWords;
-    HashSet<User> players, spymaster, red ,blue;
-    HashMap<String,Integer> scores;
-    HashMap<String,ButtonStyle> wordSets, wordsInGame;
+    HashSet<User> players, spymaster, red, blue;
+    HashMap<String, Integer> scores;
+    HashMap<String, ButtonStyle> wordSets, wordsInGame;
     HashMap<User, String> spymasterTeams;
     boolean running, ready;
     String turn;
     int guesses;
     boolean clueSent;
 
-    public Codenames(){
+    public Codenames() {
         wordList = new HashSet<>();
         customWords = new HashSet<>();
         wordSets = new HashMap<>();
@@ -40,6 +41,7 @@ public class Codenames {
     public HashSet<User> getRed() {
         return red;
     }
+
     public HashSet<User> getBlue() {
         return blue;
     }
@@ -51,6 +53,7 @@ public class Codenames {
         }
         return names;
     }
+
     public List<String> getBlueList() {
         List<String> names = new ArrayList<>();
         for (User user : blue) {
@@ -109,7 +112,7 @@ public class Codenames {
         return ready;
     }
 
-    public boolean isRunning(){
+    public boolean isRunning() {
         return running;
     }
 
@@ -130,46 +133,48 @@ public class Codenames {
     }
 
     //WordLists {
+
     /**
      * Updates the wordlist according to the sets chosen
      */
-        public void updateWordlist(){
-            wordList.clear();
-            for (String wordSet: wordSets.keySet()) {
-                if (wordSets.get(wordSet).equals(ButtonStyle.SUCCESS)){
-                    try {
-                        BufferedReader br = new BufferedReader(new FileReader(wordSet + ".txt"));
-                        String line;
-                        while((line = br.readLine()) != null) {
-                            wordList.add(line);
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
+    public void updateWordlist() {
+        wordList.clear();
+        for (String wordSet : wordSets.keySet()) {
+            if (wordSets.get(wordSet).equals(ButtonStyle.SUCCESS)) {
+                try {
+                    BufferedReader br = new BufferedReader(new FileReader(wordSet + ".txt"));
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        wordList.add(line);
                     }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
-            if(wordList.isEmpty()){
-                wordSets.put("Codenames", ButtonStyle.SUCCESS);
-                updateWordlist();
-            }
         }
+        if (wordList.isEmpty()) {
+            wordSets.put("Codenames", ButtonStyle.SUCCESS);
+            updateWordlist();
+        }
+    }
 
     /**
      * Updates the sets chosen
+     *
      * @param style Selection of the set,
      *              Chosen: ButtonStyle.SUCCESS
      *              Not Chosen: ButtonStyle.SECONDARY
      */
-        public void editWordset(String wordset, ButtonStyle style){
-            wordSets.put(wordset,style);
-        }
+    public void editWordset(String wordset, ButtonStyle style) {
+        wordSets.put(wordset, style);
+    }
 
-    public void setCustomWords(String message){
+    public void setCustomWords(String message) {
         customWords.clear();
         try {
             String[] words = message.replaceAll("[^a-zA-Z,]", " ").split(",");
             for (String word : words) {
-                word =  word.trim();
+                word = word.trim();
                 customWords.add(word);
             }
         } catch (Exception e) {
@@ -190,7 +195,7 @@ public class Codenames {
      * Resets the game,
      * Clears players and word sets chosen
      */
-    public void resetGame(){
+    public void resetGame() {
         running = true;
         players.clear();
 
@@ -204,18 +209,21 @@ public class Codenames {
 
     /**
      * Add a user to the player list
+     *
      * @param user User to add
      */
-    public void addPlayer(User user){players.add(user);}
+    public void addPlayer(User user) {
+        players.add(user);
+    }
 
     /**
      * Restores scores and words in game
      */
-    public void initialiseGame(){
+    public void initialiseGame() {
         ready = false;
         turn = "DANGER";
-        scores.put("DANGER" , 9);
-        scores.put("PRIMARY" , 8);
+        scores.put("DANGER", 9);
+        scores.put("PRIMARY", 8);
 
         wordsInGame.clear();
         updateWordlist();
@@ -224,14 +232,14 @@ public class Codenames {
 
     /**
      * Randomises the words to be used in game
+     *
      * @return Hashtable of words in game and their buttonStyles
      */
-    public HashMap<String, ButtonStyle> start(){
+    public HashMap<String, ButtonStyle> start() {
         initialiseGame();
-        Iterator<String> wordIterator = wordList.iterator();
         int i = 0;
-        while (wordIterator.hasNext() && i<25){
-            String word = wordIterator.next();
+        while (i < 25) {
+            String word = getRandomElementFromSet(wordList);
             if (i < 8) {
                 wordsInGame.put(word, ButtonStyle.PRIMARY);
             } else if (i < 17) {
@@ -246,22 +254,22 @@ public class Codenames {
         return wordsInGame;
     }
 
-    public void stop(){
+    public void stop() {
         running = false;
     }
 
 
-
     /**
      * Decrements the score of the team that last played
+     *
      * @param team DANGER = Red
      *             PRIMARY = Blue
      * @return Whether game has ended
      */
-    public boolean decrementScore(String team){
+    public boolean decrementScore(String team) {
         int newScore = -1;
-        if (!team.equals("SECONDARY")){
-            newScore = scores.get(team)-1;
+        if (!team.equals("SECONDARY")) {
+            newScore = scores.get(team) - 1;
             scores.put(team, newScore);
         }
         return newScore == 0;
@@ -270,22 +278,21 @@ public class Codenames {
     /**
      * Alternates between teams Red and Blue
      */
-    public void changeTurn(){
+    public void changeTurn() {
         turn = turn.equals("PRIMARY") ? "DANGER" : "PRIMARY";
         setClueSent(false);
     }
 
     /**
-     *
      * @param buttonId ID of the pressed button
      * @return Whether the pressed button corresponds to a button in the true layout
      */
-    public boolean checkPlayerButton(String buttonId){
+    public boolean checkPlayerButton(String buttonId) {
         String word = buttonId.replaceFirst("playerButton", "");
         return wordsInGame.containsKey(word);
     }
 
-    public ButtonStyle getButtonStyle(String buttonId){
+    public ButtonStyle getButtonStyle(String buttonId) {
         String word = buttonId.replaceFirst("playerButton", "");
         return wordsInGame.get(word);
     }
@@ -297,9 +304,9 @@ public class Codenames {
         spymaster.clear();
         red.clear();
         blue.clear();
-        Iterator<User> playerIterator = players.iterator();
-        while (playerIterator.hasNext()) {
-            User player = playerIterator.next();
+        int numPlayers = players.size();
+        for (int i = 0; i < numPlayers; i++) {
+            User player = getRandomElementFromSet(players);
             if (spymaster.size() < 2) {
                 if (spymaster.isEmpty()) {
                     spymasterTeams.put(player, "DANGER");
@@ -309,11 +316,11 @@ public class Codenames {
                 spymaster.add(player);
             } else if (red.size() < blue.size()) {
                 red.add(player);
-            } else if (red.size() > blue.size()){
+            } else if (red.size() > blue.size()) {
                 blue.add(player);
             } else {
                 Random random = new Random();
-                if (random.nextBoolean()){
+                if (random.nextBoolean()) {
                     red.add(player);
                 } else {
                     blue.add(player);
@@ -321,5 +328,17 @@ public class Codenames {
             }
         }
         ready = true;
+    }
+
+    static <E> E getRandomElementFromSet(Set<E> set) {
+        Random random = new Random();
+        int randNum = random.nextInt(set.size());
+        Iterator<E> iterator = set.iterator();
+        for (int i = 0; i < randNum; i++) {
+            iterator.next();
+        }
+        E element = iterator.next();
+        set.remove(element);
+        return element;
     }
 }
